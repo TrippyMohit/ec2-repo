6b3b9c5f6624e92708f723e97b39fbf63937a4c8
mohit <mohit@mohits-MacBook-Air.local>
2023-11-06 11:15:27 +0530

    new changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send(`bellosw, World! - ${process.env.WORD}`);
+  res.send(`ellellosw, World! - ${process.env.WORD}`);
 });
 
 app.listen(port, () => {
