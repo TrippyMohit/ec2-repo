8d7c8e7ee4d636980682eda37105020f850ea708
mohit <mohit@mohits-MacBook-Air.local>
2023-11-07 15:04:56 +0530

    new commits to subdomain

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send(`Hello, World!!!! - ${process.env.WORD}`);
+  res.send(`Hello000, World!!!! - ${process.env.WORD}`);
   // res.send(`ellellosw, World!`);
 });
 
